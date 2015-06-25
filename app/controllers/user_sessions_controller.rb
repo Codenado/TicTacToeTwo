8911d6193423d7ce202fc3645c4fc50b7f225daa
Jeffrey Leonard <jeffrey9279@gmail.com>
2015-06-24 22:36:25 -0700

    sessions controller works

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -3,8 +3,17 @@ class UserSessionsController < ApplicationController
   end
 
   def create
+  	user = User.find_by(email: params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to '/'
+    else
+      redirect_to '/login'
+    end
   end
 
-  def delete
+  def destroy
+  	session[:user_id] = nil
+    redirect_to '/'
   end
 end
